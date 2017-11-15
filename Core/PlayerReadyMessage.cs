e4789810ea102b1257773d2f81cb3b00309f05a3
Monseigneur Artous <thibaut.artis@epitech.eu>
2017-11-15 14:28:33 +0100

    some stuff

---
+++ Core/PlayerReadyMessage.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Net.NetworkInformation;
+using System.Runtime.Serialization;
+
+namespace Poker.Core.Protocol
+{
+    [DataContract]
+    public class PlayerReadyMessage : ProtocolMessage
+    {
+        [DataMember] public new const int ID = 4;
+
+        [DataMember] public string username
+        {
+            get;
+            set;
+        }
+
+        public PlayerReadyMessage() : base(ID)
+        {
+            
+        }
+
+        public PlayerReadyMessage Init()
+        {
+            return this;
+        }
+    }
+}
